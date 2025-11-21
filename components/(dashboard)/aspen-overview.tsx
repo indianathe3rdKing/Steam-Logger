ea5079a33effe6c0ea82794b81c318b4a571a8d3
indianathe3rd <indiemenziwa@gmail.com>
2025-11-21 13:02:15 +0200

    feat: add AspenOverview and FreseniusOverview components

---
+++ components/(dashboard)/aspen-overview.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AspenOverview = () => {
+  return <div>aspen-overview</div>;
+};
---
+++ components/(dashboard)/fresenius-overview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FreseniusOverview = () => {
+  return <div>fresenius-overview</div>;
+};
+
+export default FreseniusOverview;
