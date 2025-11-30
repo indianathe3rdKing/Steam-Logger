d54bfd1a5035226ca55c7504c7b88d474df0ae93
indianathe3rd <indiemenziwa@gmail.com>
2025-11-30 19:53:36 +0200

    Add Appwrite client initialization and update environment variables

--- components/appwrite.py
+++ components/appwrite.py
@@ -1,3 +1,15 @@
-import app
 
-# print("Value of __name__ in other.py: ", __name__)
+from multiprocessing import process
+
+from appwrite.client import Client
+from appwrite.services.tables_db import TablesDB
+from appwrite.id import ID
+
+# Initialize Appwrite client
+client = Client()
+client.set_endpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT)
+client.set_project(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID)
+client.set_key(process.env.APPWRITE_API_KEY)
+
+# Initialize database service
+tables_db = TablesDB(client)
