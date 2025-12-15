0814064cd762f7ab43feb098dbf64ddacc581e95
indianathe3rd <indiemenziwa@gmail.com>
2025-12-15 14:06:46 +0200

    creating excel file using openxyl

--- components/excel.py
+++ components/excel.py
@@ -1,2 +1,20 @@
 from openpyxl import Workbook, load_workbook
 import os
+
+FileName = "steam_data.xlsx"
+
+HEADERS = [
+    "Date", "Time", "Meter 1", "Bypass", "Meter Blue", "Meter Red", "Steam Flow Meter", "Aspen"
+]
+
+
+def get_workbook():
+    if os.path.exists(FileName):
+        wb = load_workbook(FileName)
+        ws = wb.active
+    else:
+        wb = Workbook()
+        ws = wb.active
+        ws.append(HEADERS)
+
+    return wb, ws
